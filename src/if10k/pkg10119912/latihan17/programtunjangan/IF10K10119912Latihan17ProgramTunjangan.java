6572f293b5940a4bd0cacb9d7fbee94ddcb8773a
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-10-26 20:26:06 +0700

    first commit

---
+++ src/if10k/pkg10119912/latihan17/programtunjangan/IF10K10119912Latihan17ProgramTunjangan.java
@@ -0,0 +1,56 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan17.programtunjangan;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ * Deskripsi Program : 
+ * 
+ */
+public class IF10K10119912Latihan17ProgramTunjangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String status;
+        float gaji, tunjangan, totalGaji;
+        
+        gaji = 0;
+        
+        Scanner input = new Scanner(System.in);
+
+        // Tampilkan output ke user
+        System.out.println("=========== Program Tunjangan ===========");
+        System.out.print("Berapa gaji poko anda perbulan ?: Rp. ");
+        gaji = input.nextInt();
+        System.out.print("Status anda ? (Menikah/belum)   : ");
+            status = input.next();
+            if(gaji >= 6000000 && status.equals("Menikah")){              
+                    tunjangan =(float) (0.35*gaji); 
+                    totalGaji = gaji+tunjangan;
+                    System.out.println("=========== Hasil Perhitungan Gaji Anda ===========");
+                    System.out.println("Gaji Pokok      : Rp. " + gaji);        
+                    System.out.println("Tunjangan       : Rp. " + tunjangan);        
+                    System.out.println("Total Gaji       : Rp. " + totalGaji);
+            }else{
+                System.out.println("=========== Hasil Perhitungan Gaji Anda ===========");
+                tunjangan = 0; 
+                System.out.println("Gaji Pokok      : Rp. " + gaji);        
+                System.out.println("Tunjangan       : Rp. " + tunjangan);        
+                System.out.println("Total Gaji       : Rp. " + gaji + tunjangan);
+            }
+
+
+        
+    }
+    
+}
